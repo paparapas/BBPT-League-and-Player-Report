85302602f541c9eaf6aa8bcf698bcd70d5375371
paparapas <andre.rendas@icloud.com>
2026-05-16 21:28:08 +0100

    Update Documentos & Calendario.py

--- pages/Documentos & Calendario.py
+++ pages/Documentos & Calendario.py
@@ -80,17 +80,17 @@ with col_dir:
 
     cartazes = [
         {
-            "nome": " Torneio Critical Hit (09/05/2026) - Liga Critical X - Link do Challonge: https://challonge.com/tournaments/signup/Xwp8IJLMCB#/signup/z3jrsguioeg",
+            "nome": " Torneio Critical Hit (16/05/2026) - Liga Critical X - Link do Challonge: https://challonge.com/pt/tournaments/signup/NLej1K5hI4#/signup/o6062vxx9tl",
             "url": "https://cdn.discordapp.com/attachments/1280979233619050660/1501703664249737216/Torneio_Critical_Hit_31_01_copy_4-1.png?ex=69ff0445&is=69fdb2c5&hm=e30239a251ffee95815947c15095990cf18c8bc72bfc0076ad03c57b0e33e239&"
         },
 
         {
-            "nome": " Torneio Mercadia (10/05/2026) - Mercadia Showdown - Link do Challonge : https://challonge.com/tournaments/signup/kyyU41xhoE#/signup/zag2e0uy3ll",
-            "url": "https://cdn.discordapp.com/attachments/1280979233619050660/1501703548315111454/Torneio_Mercadia_08_02_3.png?ex=69ff042a&is=69fdb2aa&hm=a3c2673fb947302bdad5a7ec2f8191a817bb5da7f059e8d3a5bb95521bb4ffa3&"
+            "nome": " Torneio Mercadia (17/05/2026) - Mercadia Showdown - Link do Challonge : https://challonge.com/pt/tournaments/signup/tJIucItQxF#/signup/16upmshbe0a",
+            "url": "https://cdn.discordapp.com/attachments/1280979233619050660/1504836289084788847/Torneio_Mercadia_08_02_4.png?ex=6a09c101&is=6a086f81&hm=bd2cc9dc0a5000bc17c0c89caba6ea8ec6aa4c7f01aee935399a65776114736e&"
         },
         {
-            "nome": " Liga Fénix Negra (10/05/2026) - Link do Challonge: https://challonge.com/tournaments/signup/U5VMN5OArL#/signup/ctbysplc807",
-            "url": "https://cdn.discordapp.com/attachments/1334224353142771815/1502324365969195008/4_por_5_fenix.jpg?ex=69ff4c18&is=69fdfa98&hm=5af689e9ca43e9fb7d707e59964e88530c01c85b741c4a6607fe7b1f0972a75f&"
+            "nome": " Liga Fénix Negra (17/05/2026) - Link do Challonge: https://challonge.com/pt/tournaments/signup/xH3CFyoiB2#/signup/oqhfhv4s9g",
+            "url": "https://cdn.discordapp.com/attachments/1280979233619050660/1504836287901733095/rapier.png?ex=6a09c101&is=6a086f81&hm=0b12f8d2f4cbfb4bd970960420179b43963c2970e0144703fb0e54ab5ae93f87&"
         },
     ]
     
